6dc50c676e52a735ae4c1af750521e5de27deadd
speedcubing <git@speedcubing.top>
2025-03-24 22:34:54 +0800

    add git clone into build script

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
+git clone git@github.com:speedcubing-top/main-website-backend.git
+git clone git@github.com:speedcubing-top/main-website-frontend.git
+
 sudo docker build --progress=plain -t="main-website" .
